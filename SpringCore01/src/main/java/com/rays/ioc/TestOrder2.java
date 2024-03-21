7ddfdc7fdea700549c1c095d35b62318d518e82d
HimanshuJain1991 <himanshujain119@yahoo.com>
2024-03-21 13:19:32 +0530

    done

--- SpringCore01/src/main/java/com/rays/ioc/TestOrder2.java
+++ SpringCore01/src/main/java/com/rays/ioc/TestOrder2.java
@@ -1,30 +1,18 @@
 package com.rays.ioc;
 
-public class Order2 {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-	private Payment payment;
-	private Inventory inventory;
+public class TestOrder2 {
+	public static void main(String[] args) {
 
-	// Constructor injection
-	public Order2(Payment payment, Inventory inventory) {
-		this.payment = payment;
-		this.inventory = inventory;
+		ApplicationContext context = new ClassPathXmlApplicationContext("Order2.xml");
+
+		Order2 order = (Order2) context.getBean("order2");
+
+		order.bookATicket(1);
+
 	}
-
-	public void bookATicket(int items) {
-
-		int price = 10;
-
-		double totalAmount = items * price;
-
-		double updatedBalance = payment.makePayment(totalAmount);
-
-		int updatedStock = inventory.sold(items);
-
-		System.out.println("Tickets are Booked");
-		System.out.println("Total Amount Paid: " + totalAmount);
-		System.out.println("Remaining Balance: " + updatedBalance);
-		System.out.println("Updated Stock: " + updatedStock);
-	}
+	
 
 }
